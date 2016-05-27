8a83e446ea9bf2ec041ad74edb72232d203afd73
Cristiano <christiribeiro2011@gmail.com>
2016-05-27 15:04:41 -0300

    htaccess
    comit com controller

--- application/views/index.php
+++ application/views/index.php
@@ -11,11 +11,12 @@
 
     <title>Starter Template for Bootstrap</title>
 
-    <!-- Bootstrap core CSS -->
-    <link href="dist/css/bootstrap.min.css" rel="stylesheet">
+     <!-- Bootstrap core CSS -->
+    <?php
+        echo link_tag('dist/css/bootstrap.min.css') .
+        link_tag('assets/css/starter-template.css');
+    ?>
 
-    <!-- Custom styles for this template -->
-    <link href="starter-template.css" rel="stylesheet">
   </head>
 
   <body>
@@ -50,8 +51,8 @@
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="assets/js/vendor/jquery.min.js"><\/script>')</script>
-    <script src="dist/js/bootstrap.min.js"></script>
+    <script src="<?=base_url('dist/js/bootstrap.min.js');?>"></script>
     <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
-    <script src="assets/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="<?=base_url('assets/js/ie10-viewport-bug-workaround.js');?>"></script>
   </body>
 </html>
